6e5d356e24c7fba11881eec838289ef5de084560
prashanksingh93-source <prashanksingh93@gmail.com>
2026-09-07 14:45:06 +0530

    company route

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv"
 import { connect } from "mongoose";
 import connectDB from "./utils/db.js";
 import userRoute from './routes/user.route.js'
+import companyRoute from "./routes/company.route.js"
 dotenv.config({})
 
 const app=express();
@@ -25,8 +26,9 @@ let port= process.env.PORT||3000
 //api's
 
 app.use("/api/v1/user",userRoute)
+app.use("/api/v1/company",companyRoute)
 
-http://localhost:8000/api/v1/user/register
+//http://localhost:8000/api/v1/user/register
 
 app.listen(port,()=>{
     connectDB()
---
+++ backend/routes/company.route.js
@@ -0,0 +1,13 @@
+import express, { Router } from 'express';
+import { isAuthenticated } from "../middlewares/isAuthenticated.js";
+import { getCompany, getCompanyById, registerCompany } from '../controllers/company.controller.js';
+
+const router = express.Router();
+
+router.route('/register').post(isAuthenticated,registerCompany);
+router.route('/get').get(isAuthenticated,getCompany);
+router.route('/ge/:id').get(isAuthenticated,getCompanyById)
+router.route('/update/:id').put(isAuthenticated,updateProfile);
+
+
+export default router;
