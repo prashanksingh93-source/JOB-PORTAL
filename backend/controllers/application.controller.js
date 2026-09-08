cae149fb052216846785a1624e5a469bae07aa79
prashanksingh93-source <prashanksingh93@gmail.com>
2026-09-08 17:51:37 +0530

    application controller route complete

---
+++ backend/controllers/application.controller.js
@@ -0,0 +1,130 @@
+
+
+import {Application} from "../models/application.model.js";
+import { Job } from "../models/job.model.js";
+
+export const applyJob=async(req,res)=>{
+    try{
+    const userId= req.id;
+    const jobId= req.params.id;
+    if(!jobId){
+        return res.status(400).json({
+            message:"job ID is required",
+            success:false,
+        })
+    };
+    const exitingApplication= await Application.findOne({job:jobId,applicant:userId});
+
+    if(exitingApplication){
+        return res.status(400).json({
+            message:"you have already applied for this job",
+            success:false,
+        })
+    }
+    const job= await Job.findById(jobId);
+    if(!job){
+        return res.status(404).json({
+            message:"job not found",
+            success:false,
+        })
+    };
+    const newApplication= await Application.create({
+        job:jobId,
+        applicant:userId
+    });
+    job.application.push(newApplication._id)
+    await job.save();
+    return res.status(201).json({
+        message:"job applied successfully",
+        success:true,
+        })
+    }
+    catch(error){
+     console.log(error)
+    }
+}
+
+export const getAppliedJobs=async(req,res)=>{
+    try{
+        const userId=req.id;
+        const application= await Application.find({applicant:userId}).sort({createdAt:-1}).populate({
+            path:"job",
+            option:{sort:{createdAt:-1}},
+            populate:{
+                path:'company',
+                option:{sort:{createdAt:-1}}
+            }
+        });
+        if(!application){
+            return res.status(404).json({
+                message:"No application",
+                success:false
+            })
+        };
+        return res.status(200).json({
+            application,
+            success:true,
+        })
+        
+
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+export const getApplicants= async(req,res)=>{
+    try{
+        const jobId= req.params.id;
+        const job = await Job.findById(jobId).populate({
+            path:"application",
+            option:{sort:{createdAt:-1}},
+            populate:{
+                path:"applicant"
+            }
+        });
+        if(!job){
+            return res.status(404).json({
+                message:"job not found",
+                success:false,
+            })
+        };
+        return res.status(200).json({
+            job,
+            success:true,
+        })
+
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+export const updateStatus= async(req,res)=>{
+    try{
+ const {status}=req.body;
+ const applicationId=req.params.id;
+ if(!status){
+    return res.status(400).json({
+        message:'status is required',
+        success:false,
+    })}
+    const application=await Application.findOne({_id:applicationId});
+    if(!application){
+      return res.status(404).json({
+        message:'job not found',
+        success:false
+      })
+    }
+ 
+ application.status=status.toLowerCase();
+ await application.save();
+ return res.status(200).json({
+    message:"status update successfully",
+    success:true,
+ })
+    }
+    catch(error){
+        console.log(error)
+    }
+}
--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -89,8 +89,8 @@ export const getJobById= async(req,res)=>{
 export const getAdminJobs= async(req,res)=>{
     try{
        const adminId=req.id;
-       const jobs= await Job.find({created_at:adminId});
-       if(!job){
+       const jobs= await Job.find({});
+       if(!jobs){
         return res.status(404).json({
             message:"job not found",
             success:false,
