edd243b43c108761ced642861464c80dc57b90c8
prashanksingh93-source <prashanksingh93@gmail.com>
2026-09-09 16:28:24 +0530

    frontend app created

---
+++ forntend/my-app/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import { Button } from "@/components/ui/button"
+import { ArrowUpIcon } from "lucide-react"
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+       <div className="flex flex-wrap items-center gap-2 md:flex-row">
+      <Button variant="outline">Button</Button>
+      <Button variant="outline" size="icon" aria-label="Submit">
+        <ArrowUpIcon />
+      </Button>
+    </div>
+      
+                 
+    </>
+  )
+}
+
+export default App
